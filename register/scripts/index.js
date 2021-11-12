780c9d5dbdfb1b4156a0d12f82febcca0e0a3968
2108win <dangcapcuawin@gmail.com>
2021-11-13 00:44:55 +0700

    Chinh lai 1 chut

--- register/scripts/index.js
+++ register/scripts/index.js
@@ -1,11 +1,23 @@
 //click icon to show password
 function showPassword(id, el) {
-    let x = document.getElementById(id);
-    if (x.type === "password") {
-        x.type = "text";
-        el.className = "fas fa-eye-slash showpwd";
-    } else {
-        x.type = "password";
-        el.className = "fas fa-eye showpwd";
-    }
+  let x = document.getElementById(id);
+  if (x.type === "password") {
+    x.type = "text";
+    el.className = "fas fa-eye-slash showpwd";
+  } else {
+    x.type = "password";
+    el.className = "fas fa-eye showpwd";
+  }
+}
+//kiểm tra mật khẩu trùng khớp
+function checkPassword() {
+  let password = document.getElementById("passwd1");
+  let confirmPassword = document.getElementById("passwd2");
+  if (password != confirmPassword) {
+    document.getElementsByClassName("error").innerHTML = "Mật khẩu không trùng khớp";
+    return false;
+  } else {
+    document.getElementsByClassName("error").innerHTML = "";
+    return true;
+  }
 }

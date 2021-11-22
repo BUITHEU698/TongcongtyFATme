261420e075f0b2c3d260fd515e8fc4c7d325a4af
tuan220301 <84058817+tuan220301@users.noreply.github.com>
2021-11-23 00:21:14 +0700

    chương trình khuyến mãi + mã khuyến mãi (xong)

--- edit_menu/frames/Khuyen_mai/them_ma/them_ma_js/them_ma.js
+++ edit_menu/frames/Khuyen_mai/them_ma/them_ma_js/them_ma.js
@@ -22,3 +22,7 @@ function CheckBox() {
       text.style.display = "none";
     }
   }
+  function cancle(){
+    alert("Bạn chắc chắn muốn huỷ ?")
+    location.href = "/edit_menu/frames/Khuyen_mai/Chuong_trinh_khuyen_mai/chuong_trinh_khuyen_mai.html";
+}

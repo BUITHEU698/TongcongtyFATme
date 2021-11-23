f149960bfe15c25c4fe8c4df3a426906d5b2268c
tuan220301 <84058817+tuan220301@users.noreply.github.com>
2021-11-23 21:44:21 +0700

    hoàn thành trang thêm mã khuyến mãi

--- edit_menu/frames/Khuyen_mai/them_ma/them_ma_js/them_ma.js
+++ edit_menu/frames/Khuyen_mai/them_ma/them_ma_js/them_ma.js
@@ -1,28 +1,52 @@
-function checkRadio() {
-    // document.getElementById('show').style.display = 'block';
-    var radioBox = document.getElementById("apply_4");
-    // Get the output text
-    var text = document.getElementById("show");
-    if (radioBox.checked == true){
-        text.style.display = "block";
-      } else {
-        text.style.display = "none";
-      }
-}
+// function checkRadio() {
+    
+//     var radioBox = document.getElementById("apply_4");
+    
+//     var text = document.getElementById("show");
+//     if (radioBox.checked == true){
+//         text.style.display = "block";
+//       } 
+//     else {
+//         text.style.display = "none";
+//       }
+// }
 function CheckBox() {
-    // Get the checkbox
     var checkBox = document.getElementById("showCheckBox");
-    // Get the output text
     var text = document.getElementById("show_checkbox");
-  
-    // If the checkbox is checked, display the output text
     if (checkBox.checked == true){
       text.style.display = "block";
     } else {
       text.style.display = "none";
     }
   }
-  function cancle(){
+  function CheckBox2() {
+    var checkBox2 = document.getElementById("checkBoxDanhMuc");
+    var text = document.getElementById("checkbox_danh_muc");
+    if (checkBox2.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+function cancle(){
     alert("Bạn chắc chắn muốn huỷ ?")
     location.href = "/edit_menu/frames/Khuyen_mai/Chuong_trinh_khuyen_mai/chuong_trinh_khuyen_mai.html";
 }
+$(document).ready(function() {
+  $('input[type="radio"]').click(function() {
+    if ($(this).attr('id') == 'apply_4') {
+      $('#show').show();
+    } else {
+      $('#show').hide();
+    }
+  });
+});
+// $(document).ready(function() {
+//   $('input[type="radio"]').click(function() {
+//     if ($(this).attr('id') == 'danh_muc') {
+//       $('#show2').show();
+//     } else {
+//       $('#show2').hide();
+//     }
+//   });
+// });

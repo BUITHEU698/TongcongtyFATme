10566e4532883fcbea7fc9a63761d138421e3550
phungmuppp <90637066+phungmuppp@users.noreply.github.com>
2021-11-24 02:59:44 +0700

    Create scirpt.js

---
+++ scirpt.js
@@ -0,0 +1,6 @@
+function hienthongbao(d1, d2) {
+    var v1 = document.getElementById(d1).value;
+    var v2 = document.getElementById(d2).value;
+    if (v1 > v2) alert(v1);
+    else alert(v2);
+}

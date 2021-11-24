ec9ada8edb22c7b7679b291c1bf849afc9c5246e
tuan220301 <84058817+tuan220301@users.noreply.github.com>
2021-11-24 21:16:47 +0700

    edit khuyến mãi

--- edit_menu/frames/Khuyen_mai/khuyen_mai_js/khuyen_mai.js
+++ edit_menu/frames/Khuyen_mai/khuyen_mai_js/khuyen_mai.js
@@ -9,3 +9,26 @@ var d = new Date();
 document.getElementById("today").innerHTML = today;
 document.getElementById("today2").innerHTML = today;
 document.getElementById("today3").innerHTML = today;
+
+
+$(document).ready(function() {
+  $('#select_all').on('click', function() {
+      if (this.checked) {
+          $('.checkbox').each(function() {
+              this.checked = true;
+          });
+      } else {
+          $('.checkbox').each(function() {
+              this.checked = false;
+          });
+      }
+  });
+
+  $('.checkbox').on('click', function() {
+      if ($('.checkbox:checked').length == $('.checkbox').length) {
+          $('#select_all').prop('checked', true);
+      } else {
+          $('#select_all').prop('checked', false);
+      }
+  });
+});

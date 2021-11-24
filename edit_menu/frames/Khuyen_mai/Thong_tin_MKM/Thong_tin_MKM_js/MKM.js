e8d9cfd6aaffb995e0b3a7f0cbd46bf854c0f499
tuan220301 <84058817+tuan220301@users.noreply.github.com>
2021-11-24 11:46:01 +0700

    finish 100% Khuyến mãi

---
+++ edit_menu/frames/Khuyen_mai/Thong_tin_MKM/Thong_tin_MKM_js/MKM.js
@@ -0,0 +1,41 @@
+
+function CheckBox() {
+    var checkBox = document.getElementById("showCheckBox");
+    var text = document.getElementById("show_checkbox");
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+  function CheckBox2() {
+    var checkBox2 = document.getElementById("checkBoxDanhMuc");
+    var text = document.getElementById("checkbox_danh_muc");
+    if (checkBox2.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+function cancle(){
+    alert("Bạn chắc chắn muốn huỷ ?")
+    location.href = "/edit_menu/frames/Khuyen_mai/khuyen_mai.html";
+}
+$(document).ready(function() {
+  $('input[type="radio"]').click(function() {
+    if ($(this).attr('id') == 'apply_4') {
+      $('#show').show();
+    } else {
+      $('#show').hide();
+    }
+  });
+});
+// $(document).ready(function() {
+//   $('input[type="radio"]').click(function() {
+//     if ($(this).attr('id') == 'danh_muc') {
+//       $('#show2').show();
+//     } else {
+//       $('#show2').hide();
+//     }
+//   });
+// });

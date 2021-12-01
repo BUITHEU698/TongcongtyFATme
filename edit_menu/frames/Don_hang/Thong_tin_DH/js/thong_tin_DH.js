eb58f854a13c7fe51eb59e3088b1447f1c87a3db
tuan220301 <84058817+tuan220301@users.noreply.github.com>
2021-12-01 11:45:08 +0700

    finíh don_hang

---
+++ edit_menu/frames/Don_hang/Thong_tin_DH/js/thong_tin_DH.js
@@ -0,0 +1,12 @@
+function del(){
+    if(confirm("Bạn chắc chắn muốn xoá ?"))
+        {
+            alert("Xoá thành công");
+        }
+    else{
+        alert("Đã huỷ");
+    }
+}
+function info(){
+    location.href = "/edit_menu/frames/Thong_tin_san_pham/thong_tin_san_pham.html";
+}

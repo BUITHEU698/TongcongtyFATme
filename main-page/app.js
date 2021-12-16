39cca454eec06526ca584a792099fb184c5bdae4
2108win <dangcapcuawin@gmail.com>
2021-12-16 22:01:35 +0700

    put len ne

--- main-page/app.js
+++ main-page/app.js
@@ -1,17 +1,40 @@
-// window.addEventListener("load", function () {
-//   const toggle = document.querySelector(".toggle");
-//   const menu = document.querySelector(".menu");
-//   toggle && toggle.addEventListener("click", handleToggleMenu);
-//   function handleToggleMenu(e) {
-//     menu && menu.classList.add("is-show");
-//   }
-//   document.addEventListener("click", handleClickOutside);
-//   function handleClickOutside(e) {
-//     if (e.target.matches(".toggle") || e.target.matches(".menu, .menu *"))
-//       return;
-//     menu && menu.classList.remove("is-show");
-//   }
-// });
+//scroll animation fade in
+var scroll = window.requestAnimationFrame || function (callback) {
+  window.setTimeout(callback, 1000 / 60);
+};
+var elementsToShow = document.querySelectorAll(".show-on-scroll");
+function loop() {
+  elementsToShow.forEach(function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add("is-visible");
+    } else {
+      element.classList.remove("is-visible");
+    }
+  });
+  scroll(loop);
+}
+loop();
 
-const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".menu");
+function isElementInViewport(el) {
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+// go to top button
+const gotoTop = document.querySelector(".goto-top");
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 300) {
+    gotoTop.classList.add("is-visible");
+  } else {
+    gotoTop.classList.remove("is-visible");
+  }
+});

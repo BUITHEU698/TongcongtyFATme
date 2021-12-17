ff3598278b7ef4ebe3a00c2c442dfae81ca97cf5
2108win <dangcapcuawin@gmail.com>
2021-12-17 09:03:56 +0700

    123

--- main-page/app.js
+++ main-page/app.js
@@ -31,10 +31,13 @@ function isElementInViewport(el) {
 }
 // go to top button
 const gotoTop = document.querySelector(".goto-top");
+const nav = document.querySelector(".navigation");
 window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 300) {
+  if (window.pageYOffset > 120) {
     gotoTop.classList.add("is-visible");
+    nav.classList.add("navigation-fixed");
   } else {
     gotoTop.classList.remove("is-visible");
+    nav.classList.remove("navigation-fixed");
   }
 });

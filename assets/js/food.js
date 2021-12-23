3e43505afa69b00d65ca1f38b8dbc02c0b865a28
2108win <dangcapcuawin@gmail.com>
2021-12-23 13:28:37 +0700

    7h

---
+++ assets/js/food.js
@@ -0,0 +1,9 @@
+$(".slider-responsive-banner").slick({
+  arrows: false,
+  infinite: true,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  speed: 1000,
+});

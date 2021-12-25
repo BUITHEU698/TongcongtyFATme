2192653799922bedb46729d41d0e52d05019df4a
2108win <dangcapcuawin@gmail.com>
2021-12-25 09:24:10 +0700

    trang mon an

--- assets/js/food.js
+++ assets/js/food.js
@@ -3,7 +3,58 @@ $(".slider-responsive-banner").slick({
   infinite: true,
   slidesToScroll: 1,
   adaptiveHeight: true,
-  autoplay: true,
+  autoplay: false,
   autoplaySpeed: 4000,
   speed: 1000,
+});
+$(".slider-responsive-banner-list").slick({
+  slidesToShow: 3,
+  arrows: false,
+  infinite: true,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+$(".slider-responsive-category-list").slick({
+  slidesToShow: 4,
+  arrows: false,
+  infinite: true,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 1023,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });

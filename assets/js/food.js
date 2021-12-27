0e8bba67fd12c271ad8c8d4efb984f2c60800306
2108win <dangcapcuawin@gmail.com>
2021-12-27 10:39:14 +0700

    10h

--- assets/js/food.js
+++ assets/js/food.js
@@ -11,7 +11,11 @@ $(".slider-responsive-banner-list").slick({
   slidesToShow: 3,
   arrows: false,
   infinite: true,
-  slidesToScroll: 1,
+  slidesToScroll: 2,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  variableWidth: true,
+  speed: 1000,
   responsive: [
     {
       breakpoint: 1200,
@@ -25,6 +29,7 @@ $(".slider-responsive-banner-list").slick({
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
+        variableWidth: false,
       },
     },
   ],
@@ -33,7 +38,7 @@ $(".slider-responsive-category-list").slick({
   slidesToShow: 4,
   arrows: false,
   infinite: true,
-  slidesToScroll: 1,
+  slidesToScroll: 2,
   responsive: [
     {
       breakpoint: 1200,
@@ -58,6 +63,12 @@ $(".slider-responsive-category-list").slick({
     },
   ],
 });
+$(".slider-responsive-product-list").slick({
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  variableWidth: true,
+});
 
 // get class heart click to change innerhtml image and get back
 $(".heart").click(function () {
@@ -68,3 +79,21 @@ $(".heart").click(function () {
   }
 });
 
+$("input.input-qty").each(function () {
+  var $this = $(this),
+    qty = $this.parent().find(".is-form"),
+    min = Number($this.attr("min")),
+    max = Number($this.attr("max"));
+  if (min == 0) {
+    var d = 0;
+  } else d = min;
+  $(qty).on("click", function () {
+    if ($(this).hasClass("minus")) {
+      if (d > min) d += -1;
+    } else if ($(this).hasClass("plus")) {
+      var x = Number($this.val()) + 1;
+      if (x <= max) d += 1;
+    }
+    $this.attr("value", d).val(d);
+  });
+});
